c127eb6b44f60f61e4fbcf64a6de5507497a5104
Carter Nelson <cartern@ksu.edu>
2020-03-15 17:38:50 -0500

    Image grid working.

--- client/src/components/Image.vue
+++ client/src/components/Image.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid-item" :id="id" @mouseover="mouseOver" @mouseleave="mouseLeave">
     <img :src="imgSrc" class="_img" :id="getImgId(id)"/>
-    <div class="imgText">{{ name }}</div>
+    <div class="imgText">{{ displayName }}</div>
   </div>
 </template>
 
@@ -59,21 +59,27 @@ export default {
     border:0.05em black solid;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     overflow: hidden;
+    display: grid;
+    width: 100%;
+    position: relative;
   }
 
   .imgText {
-    text-align: center;
-    position: absolute;
-    display: inline-block;
-    margin: 0 auto;
     z-index: 100;
     font-size: 2em;
     color: white;
+    margin: 0 auto;
+    grid-row: 1;
+    grid-column: 1;
+    align-self: center;
   }
 
   ._img {
+    grid-row: 1;
+    grid-column: 1;
     object-fit: cover;
-    position: relative;
+    /* position: relative; */
+    display: block;
     width: 100%;
     height: 100%;
     opacity: 0;
@@ -81,15 +87,15 @@ export default {
 
   .imgOver {
     -webkit-filter: blur(0.25em); /* Safari 6.0 - 9.0 */
-    filter: blur(0.25em);
+    filter: blur(0.25em) brightness(50%);
   }
 
   .fadeTag {
     -webkit-animation: fadein 2s forwards; /* Safari, Chrome and Opera > 12.1 */
-    -moz-animation: fadein 2s forwards; /* Firefox < 16 */
-    -ms-animation: fadein 2s forwards; /* Internet Explorer */
-      -o-animation: fadein 2s forwards; /* Opera < 12.1 */
-        animation: fadein 2s forwards;
+       -moz-animation: fadein 2s forwards; /* Firefox < 16 */
+        -ms-animation: fadein 2s forwards; /* Internet Explorer */
+         -o-animation: fadein 2s forwards; /* Opera < 12.1 */
+            animation: fadein 2s forwards;
   }
 
   @keyframes fadein {
