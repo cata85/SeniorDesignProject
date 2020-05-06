c2e31bcfd94ca2564e7d3aa9b6a19e92d87aab4b
Carter Nelson <cartern@ksu.edu>
2020-05-05 23:58:12 -0500

    Potential honeycomb background.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div id="backgroundDiv">
+      <img id="backgroundImg" src="honeycomb.png"/>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -7,5 +10,21 @@
 <style>
 #app {
   margin-top: 60px
+}
+
+#backgroundDiv {
+  position: absolute;
+  height: 20%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+}
+
+#backgroundImg {
+  height: 150%;
+  width: 100%;
+  -webkit-mask-image: linear-gradient(rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.5) 0.1%, transparent);
+  mask-image: linear-gradient(rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.5) 0.1%, transparent);
 }
 </style>
